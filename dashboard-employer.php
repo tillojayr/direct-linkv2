a78d80b504c1914e9b98839b285b13c94042b937
Jay-R Tillo <tillojayr@gmail.com>
2024-07-27 11:36:22 +0800

    update v1

--- dashboard-employer.php
+++ dashboard-employer.php
@@ -104,7 +104,7 @@
           <h1 class="text-center mb-lg-4">Job Offers</h1>
           <div class="d-grid gap-2 mb-3">
             <button class="btn btn-secondary" id="add_job_posting_btn" type="button">Add Job Posting</button>
-            </div>
+          </div>
           <div class="mb-4 btn btn-light shadow" style="background-color: #eee;">
             <div class="d-flex">
                   <!-- <div class="col-3 p-3" style="object-fit:cover;">
@@ -154,7 +154,6 @@
             <div class="col-sm-4 col-6 mb-3" style="background-color: #eee; height: 200px;">content 3</div>
           </div>
         </div>
-
       </div>
     </div>
     <div class="line my-4" style="height: 2px;
@@ -164,19 +163,62 @@
     <div class="modal fade" id="add_job_posting_modal" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
-          <div class="modal-header">
+          <div class="modal-header bg-secondary text-white">
             <h5 class="modal-title" id="exampleModalLabel1">Add Job Posting</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <form id="job_posting_form">
+            <div class="modal-body">
+              <div class="card">
+                <div class="card-body">
+                  <div class="form-group col mb-3">
+                    <label class="form-label" for="job_title">Job Title</label>
+                    <input type="text" class="form-control" id="job_title" name="job_title" required/>
+                  </div>
+                  <div class="form-group col mb-3">
+                    <label class="form-label" for="location">Location</label>
+                    <input type="text" class="form-control" id="location" name="location" required/>
+                  </div>
+                  <div class="form-group col mb-3">
+                    <label class="form-label" for="type">Type</label>
+                    <select class="form-select" name="type" id="type" required>
+                      <option value="Full Time">Full Time</option>
+                      <option value="Part Time">Part Time</option>
+                    </select>
+                  </div>
+                  <div class="form-group col mb-3">
+                    <label class="form-label" for="type1">Type1</label>
+                    <select class="form-select" name="type1" id="type1" required>
+                      <option value="Onsite">Onsite</option>
+                      <option value="Remote">Remote</option>
+                    </select>
+                  </div>
+                  <div class="form-group col mb-3">
+                    <label class="form-label" for="salary_range">Salary Range</label>
+                    <input type="text" class="form-control" id="salary_range" name="salary_range" required/>
+                  </div>
+                  <div class="mb-3">
+                      <label for="job_details" class="form-label">Job Details</label>
+                      <textarea class="form-control" id="job_details" name="job_details" rows="8" required></textarea>
+                  </div>
+                  <hr class="my-4" />
+                  <label for="requirements" class="form-label">Add Requirements</label>
+                  <div id="newinput"></div>
+                    <!-- <button id="rowAdder" type="button" class="btn btn-dark">
+                        <span class="bi bi-plus-square-dotted">
+                        </span> ADD
+                    </button> -->
+                    <div class="d-grid gap-2 mb-3">
+                      <button class="btn btn-primary" id="rowAdder" type="button">Add Job Posting</button>
+                    </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" id="add_job_posting_save_btn" class="btn btn-success px-4">Save</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -216,5 +258,7 @@
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
       integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
       crossorigin="anonymous"></script>
+
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   </body>
 </html>
---
+++ php_scripts/job-seeker/fetchJobPosting.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+// main.php
+include '../FirestoreService.php';
+
+$firestoreService = new FirestoreService();
+
+function fetchEmployerData($document_id){
+
+    $firestoreService1 = new FirestoreService();
+
+    $employers_data = $firestoreService1->fetchData('employers', $document_id);
+
+    return $employers_data;
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $jobPostings = $firestoreService->fetchDataWhere('job_postings');
+
+    foreach($jobPostings as &$jobposting){
+        // var_dump($jobposting);
+        $employers_data = fetchEmployerData($jobposting['posted_by']);
+
+        foreach($employers_data as $key => $value){
+            $jobposting[$key] = $value;
+        }
+    }
+    echo json_encode($jobPostings);
+}
+?>
